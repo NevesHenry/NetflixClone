5605ffb315fc2ed752765b88aa54d5363a50c4bc
NevesHenry <henriquereisneves@hotmail.com>
2023-01-20 00:41:59 -0800

    First Commit

---
+++ public/js/api.js
@@ -0,0 +1,6 @@
+let api_key = "0cd40a2968752023251464951e88c23e";
+let image_url = "https://image.tmdb.org/t/p/w500";
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
+
+
